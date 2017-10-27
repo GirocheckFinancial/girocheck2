ab4bf4aa3ee229fab7c86aa30229df1abc12acc5
Roberto Rodriguez <robertosoftwareengineer@gmail.com>
2017-10-27 09:03:35 -0400

    10-26-dev

--- AMS2/src/main/webapp/app/cmp/toolbar/PdfReportButton.js
+++ AMS2/src/main/webapp/app/cmp/toolbar/PdfReportButton.js
@@ -7,11 +7,18 @@ Ext.define('Admin.cmp.toolbar.PdfReportButton', {
         click: function (button) {
             var grid = button.up().up().items.items[0],
                     gridTab = grid.up(),
-                    params = grid.getParams(), 
+                    params = grid.getParams(),
                     entity = gridTab.entity,
                     url = "/AMS2/report/" + entity + ".htm?params=" + params;
-            console.log(url);
-            window.open(url , 'window', 'HEIGHT=660,resizable=yes,scrollbars=yes,WIDTH=800,target="blank_"');
+            var total = grid.getStore().totalCount;
+            if (total > 10) {
+                if (confirm('You are about generate a report of of ' + total + ' records. Do you want to proceed?')) {
+                    window.open(url, 'window', 'HEIGHT=660,resizable=yes,scrollbars=yes,WIDTH=800,target="blank_"');
+                }
+            } else {
+                window.open(url, 'window', 'HEIGHT=660,resizable=yes,scrollbars=yes,WIDTH=800,target="blank_"');
+            }
+
         }
     }
 });
